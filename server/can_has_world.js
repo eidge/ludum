18caedef74f255eee44e792c4f96e4e26b243f6a
Miguel Antunes <mantunes@redlightsoft.com>
2014-12-06 01:39:35 +0000

    added world chaos generation

---
+++ server/can_has_world.js
@@ -0,0 +1,5 @@
+//generates a word map
+// { width: "1980", height: "1024", bounds: [{x:"6" , y:"6", tile:"tile_id_47emeio"}, {x:"6" , y:"7", tile:"tile_id_48emeio"}]}
+plzPlzPlzCanIHasThemWorld() {
+  
+}
